055b4ab6dbf01b17214a81d6a22844e500c79acb
kwakkun2002 <kwakkun2002@gmail.com>
2021-09-05 18:35:14 +0900

    periodic spike 구조 변경
    Obstacle을 붙여서 작동하도록 변경
    Periodic Spike Condition, Periodic Spike Action 붙여야 작동

---
+++ Assets/Scripts/Obstacle/Spike/PeriodicSpikeAction.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Obstacle.Spike
+{
+    public class PeriodicSpikeAction : MonoBehaviour, IAction
+    {
+        public int ActivateTime;
+        public GameObject prefabObj;
+        private GameObject obj;
+
+        public void DoAction()
+        {
+            if (!GameObject.Find("SpikeClone"))
+            {
+                obj = Instantiate(prefabObj);
+                obj.name = "SpikeClone";
+                Vector3 pos = new Vector3(transform.position.x, transform.position.y + 1, 0);
+                obj.transform.position = pos;
+                Destroy(obj, ActivateTime);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Obstacle/Spike/PeriodicSpikeCondition.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Obstacle.Spike
+{
+    public class PeriodicSpikeCondition : MonoBehaviour, ICondition
+    {
+        private bool IsActivated { get; set; }
+        public int DeactivateTime;
+        
+        void Start()
+        {
+            InvokeRepeating(nameof(ReverseIsActivated), DeactivateTime, DeactivateTime);
+        }
+
+        void ReverseIsActivated()
+        {
+            IsActivated = !IsActivated;
+        }
+        
+        public bool CheckCondition()
+        {
+            return IsActivated;
+        }
+    }
+}
